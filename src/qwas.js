e95bdbb72e965d35f925ff6e2440a6b3c62c8e01
Jeremy Carter <Jeremy@JeremyCarter.ca>
2016-10-12 08:59:02 -0400

    Add the pieces

---
+++ src/qwas.js
@@ -0,0 +1,42 @@
+function Qwas(x, y, rot, w, h, density, friction, restitution) {
+	
+	this.x = x;
+	this.y = y;
+	this.rot = rot;
+	this.w = w;
+	this.h = h;
+	this.density = density;
+	this.friction = friction;
+	this.restitution = restitution;
+	
+	var scale = 40;
+	
+	this.vertices = [
+         -0.5 * scale, 0.5 * scale,
+         0.5 * scale, 0.5 * scale,
+         0.5 * scale, -0.5 * scale,
+         -0.5 * scale, -0.5 * scale
+//         -0.25 * scale, -0.75 * scale,
+//         -0.75 * scale, -0.75 * scale,
+//         -0.75 * scale, 0.75 * scale
+    ];
+	
+	// TODO: Fix this
+	this.shape = [
+        [
+	        [-0.75 * scale, -0.25 * scale],
+	        [0.75 * scale, -0.25 * scale],
+	        [0.75 * scale, 0.25 * scale],
+	        [-0.75 * scale, 0.25 * scale]
+        ],
+        [
+			[0.25 * scale, 0.25 * scale],
+			[0.75 * scale, 0.25 * scale],
+			[0.75 * scale, 0.75 * scale],
+			[0.25 * scale, 0.75 * scale]
+        ]
+    ];
+	
+	this.colour = 0xAA66FF;
+}
+
--- src/stacktris.js
+++ src/stacktris.js
@@ -5,30 +5,61 @@ function Stacktris() {
 Stacktris.prototype.start = function() {
 	console.log('hello');
 	
-	this.renderer = new PIXI.WebGLRenderer(800, 600);
+	this.renderer = new PIXI.WebGLRenderer(400, 600);
 	document.body.appendChild(this.renderer.view);
 	
-	this.stage = new PIXI.Container();
 	
-	this.bunny = null;
 	
-	PIXI.loader.add('bunny', 'bunny.png').load((function(loader, resources) {
-	    // This creates a texture from a 'bunny.png' image.
-	    this.bunny = new PIXI.Sprite(resources.bunny.texture);
-
-	    // Setup the position and scale of the bunny
-	    this.bunny.position.x = 400;
-	    this.bunny.position.y = 300;
-
-	    this.bunny.scale.x = 2;
-	    this.bunny.scale.y = 2;
-
-	    // Add the bunny to the scene we are building.
-	    this.stage.addChild(this.bunny);
-
-	    // kick off the animation loop (defined below)
-	    this.animate();
-	}).bind(this));
+//	this.graphics = new PIXI.Graphics();
+//	this.graphics.beginFill(0xFFFF00);
+//	this.graphics.lineStyle(0, 0xFF0000);
+//	this.graphics.drawRect(0, 0, 300, 200);
+//	this.stage.addChild(this.graphics);
+	
+	var break_out = false;
+	while (!break_out) {
+		this.stage = new PIXI.Container();
+		this.angles = [
+           0.0, 
+           90.0, 
+           180.0, 
+           270.0
+        ];
+		this.shuffle(this.angles);
+		//console.log(angles);
+		var a = this.angles[Math.floor(Math.random()) % this.angles.length];
+		
+		this.possibleObjects = [
+            new Asdf(100, 100, a),
+            new Easd(100, 100, a),
+            new Qasd(100, 100, a),
+            new Qwas(100, 100, a),
+            new Wasd(100, 100, a)
+        ];
+		
+		this.shuffle(this.possibleObjects);
+		
+		var obj = this.possibleObjects[Math.floor(Math.random()) % this.possibleObjects.length];
+		
+		console.log(obj);
+		
+		var g = new PIXI.Graphics();
+		g.beginFill(obj.colour);
+		g.lineStyle(0, 0xFF0000);
+		g.position.x = obj.x;
+		g.position.y = obj.y;
+		g.rotation = a * Math.PI / 180.0;
+		g.drawPolygon(obj.vertices);
+		this.stage.addChild(g);
+		
+		this.objects = [
+            obj
+        ];
+		
+		break;
+	}
+	
+	this.animate();
 };
 
 Stacktris.prototype.animate = function() {
@@ -36,8 +67,18 @@ Stacktris.prototype.animate = function() {
     requestAnimationFrame(this.animate.bind(this));
 
     // each frame we spin the bunny around a bit
-    this.bunny.rotation += 0.01;
+//    this.bunny.rotation += 0.01;
 
     // this is the main render call that makes pixi draw your container and its children.
     this.renderer.render(this.stage);
-}
+};
+
+Stacktris.prototype.shuffle = function(a) {
+	var j, x, i;
+	for (i = a.length; i; i--) {
+		j = Math.floor(Math.random() * i);
+		x = a[i - 1];
+		a[i - 1] = a[j];
+		a[j] = x;
+	}
+};
