be62e92570bbe90fd6e630597dbc0e54acfc51ca
Jeremy Carter <Jeremy@JeremyCarter.ca>
2016-10-13 13:37:07 -0400

    Convex newdrop works
    Concave newdrop doesn't show the piece until it settles for some reason

--- src/stacktris.js
+++ src/stacktris.js
@@ -7,7 +7,6 @@ function Stacktris() {
 }
 
 Stacktris.prototype.start = function() {
-//	console.log('hello');
 	
 	this.renderer = new PIXI.WebGLRenderer(400, 600);
 	document.body.appendChild(this.renderer.view);
@@ -25,17 +24,17 @@ Stacktris.prototype.start = function() {
 	var a = this.angles[Math.floor(Math.random()) % this.angles.length];
 	
 	this.possibleObjects = [
-        new Asdf(this.w/2 - 25, 0, a),
-        new Easd(this.w/2 - 25, 0, a),
-        new Qasd(this.w/2 - 25, 0, a),
-        new Qwas(this.w/2 - 25, 0, a),
-        new Wasd(this.w/2 - 25, 0, a)
+        Asdf,
+        Qwas,
+//        Easd,
+//        Qasd,
+//        Wasd
     ];
 	
 	this.shuffle(this.possibleObjects);
 	
 	
-	var obj = this.possibleObjects[Math.floor(Math.random()) % this.possibleObjects.length]; 
+	var obj = new (this.possibleObjects[Math.floor(Math.random()) % this.possibleObjects.length])(this.w/2 - 25, 0.0, a);
 	
 	var g = new PIXI.Graphics();
 	g.beginFill(obj.colour);
@@ -45,21 +44,6 @@ Stacktris.prototype.start = function() {
 	g.drawPolygon(obj.vertices);
 	this.stage.addChild(g);
 	obj.g = g;
-	
-//	this.shuffle(this.possibleObjects);
-//	
-//	var obj2 = this.possibleObjects[Math.floor(Math.random()) % this.possibleObjects.length]; 
-//	console.log(obj2);
-//	
-//	var g2 = new PIXI.Graphics();
-//	g2.beginFill(obj2.colour);
-//	g2.lineStyle(0, 0xFF0000);
-//	g2.position.x = obj2.x;
-//	g2.position.y = obj2.y;
-//	g2.drawPolygon(obj2.vertices);
-//	this.stage.addChild(g2);
-//	obj2.g = g2;
-	
 	
 	this.objects = [
         obj
@@ -72,7 +56,6 @@ Stacktris.prototype.start = function() {
 
 Stacktris.prototype.animate = function() {
 	
-//	this.initPhysics(true);
 	this.stepPhysics();
 	
     // start the timer for the next animation loop
@@ -86,37 +69,24 @@ Stacktris.prototype.initPhysics = function() {
 	
 	var restitution = 0.3;
 	
-//	if (!update) {
-		
-//		console.log('not update');
+	this.dynamicBodies = [];
+
+	var groundBodyDef = new Box2D.Dynamics.b2BodyDef();
+	groundBodyDef.position.Set(0.0, 600.0);
+	var gravity = new Box2D.Common.Math.b2Vec2(0.0, 100.0);
+	this.world = new Box2D.Dynamics.b2World(gravity);
+	var groundBody = this.world.CreateBody(groundBodyDef);
+	var groundBodyShape = new Box2D.Collision.Shapes.b2PolygonShape();
+	groundBodyShape.SetAsBox(500.0, 0.0);
+	var groundBodyFixtureDef = new Box2D.Dynamics.b2FixtureDef();
+	groundBodyFixtureDef.shape = groundBodyShape;
+	groundBodyFixtureDef.density = 1.0;
+	groundBodyFixtureDef.friction = 1.0;
 	
-		this.dynamicBodies = [];
-//	}
-	
-		var groundBodyDef = new Box2D.Dynamics.b2BodyDef();
-	//	console.log(groundBodyDef);
-		groundBodyDef.position.Set(0.0, 600.0);
-		var gravity = new Box2D.Common.Math.b2Vec2(0.0, 100.0);
-		this.world = new Box2D.Dynamics.b2World(gravity);
-		var groundBody = this.world.CreateBody(groundBodyDef);
-		var groundBodyShape = new Box2D.Collision.Shapes.b2PolygonShape();
-		groundBodyShape.SetAsBox(500.0, 0.0);
-		var groundBodyFixtureDef = new Box2D.Dynamics.b2FixtureDef();
-		groundBodyFixtureDef.shape = groundBodyShape;
-		groundBodyFixtureDef.density = 1.0;
-		groundBodyFixtureDef.friction = 1.0;
-		
-		groundBody.CreateFixture(groundBodyFixtureDef);
-//	}
+	groundBody.CreateFixture(groundBodyFixtureDef);
 
 	for (var i = 0; i < this.objects.length; i++) {
 		var obj = this.objects[i];
-		
-//		if (update) {
-//			console.log('update');
-//			obj.x = this.w/2 - 25;
-//			obj.y = 0.0;
-//		}
 		
 		var dynamicBodyDef = new Box2D.Dynamics.b2BodyDef();
 		dynamicBodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
@@ -136,9 +106,6 @@ Stacktris.prototype.initPhysics = function() {
 			for (var j = 0; j < obj.shapes[0].length; j++) {
 				
 				var shape = obj.shapes[0][j];
-				
-//				console.log('! shape !');
-//				console.log(shape);
 				
 				vertices.push(
 					new Box2D.Common.Math.b2Vec2(
@@ -167,19 +134,14 @@ Stacktris.prototype.initPhysics = function() {
 			
 			for (var j = 0; j < obj.shapes.length; j++) {
 			
-				var shape = obj.shapes[j];
+				var shapes = obj.shapes[j];
 				var dynamicBodyShape = new Box2D.Collision.Shapes.b2PolygonShape();
 				
 				var vertices = [];
 				
-				for (var k = 0; k < obj.shapes[j].length; k++) {
+				for (var k = 0; k < shapes.length; k++) {
 					
-//					console.log('k ' + k);
-					
-					var shape = obj.shapes[j][k];
-					
-//					console.log('! shape !');
-//					console.log(shape);
+					var shape = shapes[k];
 					
 					vertices.push(
 						new Box2D.Common.Math.b2Vec2(
@@ -191,7 +153,6 @@ Stacktris.prototype.initPhysics = function() {
 				
 				dynamicBodyShape.SetAsArray(vertices, vertices.length);
 				
-//				dynamicBodyShape.Set(shape[0], shape.length);
 				var dynamicBodyFixtureDef = new Box2D.Dynamics.b2FixtureDef();
 				dynamicBodyFixtureDef.shape = dynamicBodyShape;
 				dynamicBodyFixtureDef.density = 1.0;
@@ -210,7 +171,6 @@ Stacktris.prototype.initPhysics = function() {
 			dynamicBodyShape.SetAsBox(10.0, 10.0);
 
 			var vertices = [];
-//			console.log(obj.vertices);
 			for (var j = 0; j < obj.vertices.length; j++)
 			{
 				vertices.push({
@@ -241,63 +201,82 @@ Stacktris.prototype.initPhysics = function() {
 	this.world.SetContactListener(new MyContactListener());
 };
 
-Stacktris.prototype.updatePhysics = function(objects) {
+Stacktris.prototype.updatePhysics = function() {
 	
 	var restitution = 0.3;
 	
-//	if (!update) {
-		
-//		console.log('not update');
+	var obj = this.objects[this.objects.length-1];
 	
-//		this.dynamicBodies = [];
-//	}
+	obj.x = this.w/2 - 25;
+	obj.y = 0.0;
 	
-//		var groundBodyDef = new Box2D.Dynamics.b2BodyDef();
-//	//	console.log(groundBodyDef);
-//		groundBodyDef.position.Set(0.0, 600.0);
-//		var gravity = new Box2D.Common.Math.b2Vec2(0.0, 100.0);
-//		this.world = new Box2D.Dynamics.b2World(gravity);
-//		var groundBody = this.world.CreateBody(groundBodyDef);
-//		var groundBodyShape = new Box2D.Collision.Shapes.b2PolygonShape();
-//		groundBodyShape.SetAsBox(500.0, 0.0);
-//		var groundBodyFixtureDef = new Box2D.Dynamics.b2FixtureDef();
-//		groundBodyFixtureDef.shape = groundBodyShape;
-//		groundBodyFixtureDef.density = 1.0;
-//		groundBodyFixtureDef.friction = 1.0;
-//		
-//		groundBody.CreateFixture(groundBodyFixtureDef);
-//	}
-
-	for (var i = 0; i < objects.length; i++) {
-		var obj = objects[i];
+//		console.log(obj);
 		
-//		if (update) {
-//			console.log('update');
-			obj.x = this.w/2 - 25;
-			obj.y = 0.0;
-//		}
-		
-		var dynamicBodyDef = new Box2D.Dynamics.b2BodyDef();
-		dynamicBodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
-		dynamicBodyDef.position = new Box2D.Common.Math.b2Vec2(obj.x, obj.y);
-		dynamicBodyDef.angle = obj.rot * Math.PI / 180.0;
+//		var g = new PIXI.Graphics();
+//		g.beginFill(obj.colour);
+//		g.lineStyle(0, 0xFF0000);
+	obj.g.position.x = this.w/2 - 25;
+	obj.g.position.y = 0.0;
+//		obj.g.drawPolygon(obj.vertices);
+//		this.stage.addChild(g);
+//		obj.g = g;
+	
+	var dynamicBodyDef = new Box2D.Dynamics.b2BodyDef();
+	dynamicBodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
+	dynamicBodyDef.position = new Box2D.Common.Math.b2Vec2(this.w/2 - 25, 0.0);
+	dynamicBodyDef.angle = obj.rot * Math.PI / 180.0;
 //		dynamicBodyDef.set_bullet(true);
-		var dynamicBody = this.world.CreateBody(dynamicBodyDef);
+	var dynamicBody = this.world.CreateBody(dynamicBodyDef);
+	
+	console.log(dynamicBodyDef.position);
+	
+	if (obj.shapes && obj.shapes.length == 1) {	// If object is convex.
 		
-		if (obj.shapes && obj.shapes.length == 1) {	// If object is convex.
+		console.log('convex');
+		
+		var dynamicBodyShape = new Box2D.Collision.Shapes.b2PolygonShape();
+		
+		var vertices = [];
+		
+		for (var j = 0; j < obj.shapes[0].length; j++) {
 			
-			console.log('convex');
+			var shape = obj.shapes[0][j];
 			
+			vertices.push(
+				new Box2D.Common.Math.b2Vec2(
+					shape[0],
+					shape[1]
+				)
+			);
+		}
+		
+		dynamicBodyShape.SetAsArray(vertices, vertices.length);
+		
+		
+		var dynamicBodyFixtureDef = new Box2D.Dynamics.b2FixtureDef();
+		dynamicBodyFixtureDef.shape = dynamicBodyShape;
+
+		dynamicBodyFixtureDef.density = 1.0;
+		dynamicBodyFixtureDef.friction = 1.0;
+		dynamicBodyFixtureDef.restitution = restitution;
+		dynamicBodyFixtureDef.userData = 1;	
+		dynamicBody.CreateFixture(dynamicBodyFixtureDef);
+		this.dynamicBodies.push(dynamicBody);
+		
+	} else if (obj.shapes && obj.shapes.length > 1) {	// If object is concave.
+		
+		console.log('concave');
+		
+		for (var j = 0; j < obj.shapes.length; j++) {
+		
+			var shapes = obj.shapes[j];
 			var dynamicBodyShape = new Box2D.Collision.Shapes.b2PolygonShape();
 			
 			var vertices = [];
 			
-			for (var j = 0; j < obj.shapes[0].length; j++) {
+			for (var k = 0; k < shapes.length; k++) {
 				
-				var shape = obj.shapes[0][j];
-				
-//				console.log('! shape !');
-//				console.log(shape);
+				var shape = shapes[k];
 				
 				vertices.push(
 					new Box2D.Common.Math.b2Vec2(
@@ -309,128 +288,66 @@ Stacktris.prototype.updatePhysics = function(objects) {
 			
 			dynamicBodyShape.SetAsArray(vertices, vertices.length);
 			
-			
-			var dynamicBodyFixtureDef = new Box2D.Dynamics.b2FixtureDef();
-			dynamicBodyFixtureDef.shape = dynamicBodyShape;
-
-			dynamicBodyFixtureDef.density = 1.0;
-			dynamicBodyFixtureDef.friction = 1.0;
-			dynamicBodyFixtureDef.restitution = restitution;
-			dynamicBodyFixtureDef.userData = 1;	
-			dynamicBody.CreateFixture(dynamicBodyFixtureDef);
-			this.dynamicBodies.push(dynamicBody);
-			
-		} else if (obj.shapes && obj.shapes.length > 1) {	// If object is concave.
-			
-			console.log('concave');
-			
-			for (var j = 0; j < obj.shapes.length; j++) {
-			
-				var shape = obj.shapes[j];
-				var dynamicBodyShape = new Box2D.Collision.Shapes.b2PolygonShape();
-				
-				var vertices = [];
-				
-				for (var k = 0; k < obj.shapes[j].length; k++) {
-					
-//					console.log('k ' + k);
-					
-					var shape = obj.shapes[j][k];
-					
-//					console.log('! shape !');
-//					console.log(shape);
-					
-					vertices.push(
-						new Box2D.Common.Math.b2Vec2(
-							shape[0],
-							shape[1]
-						)
-					);
-				}
-				
-				dynamicBodyShape.SetAsArray(vertices, vertices.length);
-				
-//				dynamicBodyShape.Set(shape[0], shape.length);
-				var dynamicBodyFixtureDef = new Box2D.Dynamics.b2FixtureDef();
-				dynamicBodyFixtureDef.shape = dynamicBodyShape;
-				dynamicBodyFixtureDef.density = 1.0;
-				dynamicBodyFixtureDef.friction = 1.0;
-				dynamicBodyFixtureDef.restitution = restitution;
-				dynamicBodyFixtureDef.userData = 1;
-				
-				dynamicBody.CreateFixture(dynamicBodyFixtureDef);
-				this.dynamicBodies.push(dynamicBody);
-			}
-		} else if (!obj.shapes || (obj.shapes && obj.shapes.length < 1)) {	// If object has no shapes it must be a triangle.
-			
-			console.log('triangle');
-			
-			var dynamicBodyShape = new Box2D.Collision.Shapes.b2PolygonShape();
-			dynamicBodyShape.SetAsBox(10.0, 10.0);
-
-			var vertices = [];
-//			console.log(obj.vertices);
-			for (var j = 0; j < obj.vertices.length; j++)
-			{
-				vertices.push({
-					x: obj.vertices[j].x,
-					y: obj.vertices[j].y
-				});
-			}
-
-			dynamicBodyShape.Set(vertices, obj.vertices.length);
-
 			var dynamicBodyFixtureDef = new Box2D.Dynamics.b2FixtureDef();
 			dynamicBodyFixtureDef.shape = dynamicBodyShape;
 			dynamicBodyFixtureDef.density = 1.0;
 			dynamicBodyFixtureDef.friction = 1.0;
 			dynamicBodyFixtureDef.restitution = restitution;
 			dynamicBodyFixtureDef.userData = 1;
-
-			dynamicBody.CreateFixture(dynamicBodyFixtureDef);
 			
+			dynamicBody.CreateFixture(dynamicBodyFixtureDef);
 			this.dynamicBodies.push(dynamicBody);
 		}
+	} else if (!obj.shapes || (obj.shapes && obj.shapes.length < 1)) {	// If object has no shapes it must be a triangle.
 		
+		console.log('triangle');
 		
+		var dynamicBodyShape = new Box2D.Collision.Shapes.b2PolygonShape();
+		dynamicBodyShape.SetAsBox(10.0, 10.0);
+
+		var vertices = [];
+		for (var j = 0; j < obj.vertices.length; j++)
+		{
+			vertices.push({
+				x: obj.vertices[j].x,
+				y: obj.vertices[j].y
+			});
+		}
+
+		dynamicBodyShape.Set(vertices, obj.vertices.length);
+
+		var dynamicBodyFixtureDef = new Box2D.Dynamics.b2FixtureDef();
+		dynamicBodyFixtureDef.shape = dynamicBodyShape;
+		dynamicBodyFixtureDef.density = 1.0;
+		dynamicBodyFixtureDef.friction = 1.0;
+		dynamicBodyFixtureDef.restitution = restitution;
+		dynamicBodyFixtureDef.userData = 1;
+
+		dynamicBody.CreateFixture(dynamicBodyFixtureDef);
 		
-		obj.visible = true;
+		this.dynamicBodies.push(dynamicBody);
 	}
 	
-//	this.world.SetContactListener(new MyContactListener());
+	obj.visible = true;
 };
 
 Stacktris.prototype.stepPhysics = function() {
 	
-	var timeStep = 1.0 / 60.0;
-	var velocityIterations = 6;
-	var positionIterations = 2;
+	console.log('yvel: ' + this.dynamicBodies[this.dynamicBodies.length-1].GetLinearVelocity().y);
+	console.log('pos: ' + this.dynamicBodies[this.dynamicBodies.length-1].GetPosition().y);
 	
-	this.world.Step(timeStep, velocityIterations, positionIterations);
-	this.world.ClearForces();
-	
-	for (var i = 0; i < this.objects.length; i++) {
-		var obj = this.objects[i];
+	if (this.dynamicBodies[this.dynamicBodies.length-1].GetPosition().y > 0.1 && this.dynamicBodies[this.dynamicBodies.length-1].GetLinearVelocity().y <= 4.0 && this.dynamicBodies[this.dynamicBodies.length-1].GetLinearVelocity().x <= 0.0) {
 		
-		var dynamicBodyPosition = this.dynamicBodies[i].GetPosition();
-//		console.log(dynamicBodyPosition);
-		var dynamicBodyAngle = this.dynamicBodies[i].GetAngle() * 180.0 / Math.PI;
-//		console.log(dynamicBodyAngle);
-		obj.x = dynamicBodyPosition.x;
-		obj.y = dynamicBodyPosition.y;
-		obj.rot = Math.fmod(dynamicBodyAngle, 360.0);
-		obj.g.position.x = obj.x;
-		obj.g.position.y = obj.y;
-		obj.g.rotation = obj.rot * Math.PI / 180.0;
-	}
-	
-//	console.log(this.dynamicBodies[this.dynamicBodies.length-1].GetLinearVelocity().y);
-	
-	if (this.dynamicBodies[this.dynamicBodies.length-1].GetLinearVelocity().y <= 0.0) {
-		var obj = this.possibleObjects[Math.floor(Math.random()) % this.possibleObjects.length]; 
+		console.log('settled');
+		
+		var a = this.angles[Math.floor(Math.random()) % this.angles.length];
+		
+		var obj = new (this.possibleObjects[Math.floor(Math.random()) % this.possibleObjects.length])(this.w/2 - 25, 0.0, a); 
 		
 		obj.x = this.w/2 - 25;
 		obj.y = 0.0;
+		
+		obj.visible = true;
 		
 		var g = new PIXI.Graphics();
 		g.beginFill(obj.colour);
@@ -441,8 +358,31 @@ Stacktris.prototype.stepPhysics = function() {
 		this.stage.addChild(g);
 		obj.g = g;
 		this.objects.push(obj);
-		this.updatePhysics(this.objects);
+		this.updatePhysics();
+		
+		this.renderer.render(this.stage);
+		return;
 	}
+	
+	var timeStep = 1.0 / 60.0;
+	var velocityIterations = 6;
+	var positionIterations = 2;
+	
+	this.world.Step(timeStep, velocityIterations, positionIterations);
+	this.world.ClearForces();
+	
+		for (var i = 0; i < this.objects.length; i++) {
+			var obj = this.objects[i];
+			
+			var dynamicBodyPosition = this.dynamicBodies[i].GetPosition();
+			var dynamicBodyAngle = this.dynamicBodies[i].GetAngle() * 180.0 / Math.PI;
+			obj.x = dynamicBodyPosition.x;
+			obj.y = dynamicBodyPosition.y;
+			obj.rot = Math.fmod(dynamicBodyAngle, 360.0);
+			obj.g.position.x = obj.x;
+			obj.g.position.y = obj.y;
+			obj.g.rotation = obj.rot * Math.PI / 180.0;
+		}
 };
 
 Stacktris.prototype.shuffle = function(a) {
